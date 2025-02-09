b298f3356827cb4c445c8693378a9ddcb34a7a1f
brian <dpi.studio@gmail.com>
2025-02-09 11:59:03 +0800

    完成 UI 設計與變數命名

--- CSA05/Form1.Designer.cs
+++ CSA05/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labTitle = new System.Windows.Forms.Label();
+            this.labAccount = new System.Windows.Forms.Label();
+            this.labPassowrd = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labTitle
+            // 
+            this.labTitle.AutoSize = true;
+            this.labTitle.Font = new System.Drawing.Font("Microsoft JhengHei", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labTitle.Location = new System.Drawing.Point(210, 84);
+            this.labTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labTitle.Name = "labTitle";
+            this.labTitle.Size = new System.Drawing.Size(145, 40);
+            this.labTitle.TabIndex = 0;
+            this.labTitle.Text = "帳號註冊";
+            // 
+            // labAccount
+            // 
+            this.labAccount.AutoSize = true;
+            this.labAccount.Location = new System.Drawing.Point(61, 147);
+            this.labAccount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labAccount.Name = "labAccount";
+            this.labAccount.Size = new System.Drawing.Size(64, 23);
+            this.labAccount.TabIndex = 1;
+            this.labAccount.Text = "帳號：";
+            // 
+            // labPassowrd
+            // 
+            this.labPassowrd.AutoSize = true;
+            this.labPassowrd.Location = new System.Drawing.Point(61, 204);
+            this.labPassowrd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labPassowrd.Name = "labPassowrd";
+            this.labPassowrd.Size = new System.Drawing.Size(64, 23);
+            this.labPassowrd.TabIndex = 2;
+            this.labPassowrd.Text = "密碼：";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(132, 144);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(314, 31);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(132, 201);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(314, 31);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(131, 279);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(106, 46);
+            this.btnRegister.TabIndex = 5;
+            this.btnRegister.Text = "註冊";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(340, 279);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(106, 46);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(554, 405);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.labPassowrd);
+            this.Controls.Add(this.labAccount);
+            this.Controls.Add(this.labTitle);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "帳號註冊";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labTitle;
+        private System.Windows.Forms.Label labAccount;
+        private System.Windows.Forms.Label labPassowrd;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
 
