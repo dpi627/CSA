67e79e03fe354b810866a98a6b0bef1dadc52b07
brian <dpi.studio@gmail.com>
2025-02-09 11:24:31 +0800

    新增銀行名稱判斷

--- CSA01/Program.cs
+++ CSA01/Program.cs
@@ -10,5 +10,22 @@ namespace CSA01
             var first2Code = Console.ReadLine();
             Console.WriteLine(first2Code);
         }
+
+        static string GetBankName(string first2Code)
+        {
+            switch (first2Code)
+            {
+                case "03":
+                    return "此卡片為聯合信用卡";
+                case "35":
+                    return "此卡片為JCB信用卡";
+                case "45":
+                    return "此卡片為MASTER信用卡";
+                case "54":
+                    return "此卡片為VISA信用卡";
+                default:
+                    return "非本店特約用戶";
+            }
+        }
     }
 }
