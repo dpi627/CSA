734d271b4d3b904a98c566c7c83c2c1cf78cda54
Brian <dpi.studio@gmail.com>
2025-02-09 19:07:00 +0800

    新增密碼加密方法 註冊方法進行密碼加密 完成註冊按鈕方法，並顯示訊息

--- CSA05/Form1.cs
+++ CSA05/Form1.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
 
 namespace CSA05
 {
     public partial class Form1 : Form
     {
+        private readonly DAC _dac;
+
         public Form1()
         {
             InitializeComponent();
+            _dac = new DAC();
         }
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-
+            _dac.Register(txtUserName.Text, txtPassword.Text);
+            MessageBox.Show("註冊成功");
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
@@ -34,7 +40,19 @@ namespace CSA05
 
         public void Register(string user, string password)
         {
-            File.AppendAllText(_path, $"{user}|{password}\n");
+            string encryptedPassword = EncryptPassword(password);
+            File.AppendAllText(_path, $"{user}|{encryptedPassword}\n");
+        }
+
+        private string EncryptPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hash = BitConverter.ToString(bytes).Replace("-", "");  // 轉成十六進制字串
+                return hash;
+            }
+            // BitConverter.ToString(hashBytes) 預設會在每個十六進制數值之間加上 -（連字號），所以用 .Replace("-", "") 來移除，使結果更簡潔
         }
     }
 }
